091566e8d993566c356ae77f291bcf50266b7859
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2018-03-11 21:51:44 +0100

    Practise2

---
+++ src/practise_2/example/domain/Engineer.java
@@ -0,0 +1,8 @@
+package practise_2.example.domain;
+
+public class Engineer extends Employee {
+
+    public Engineer(int empId, String name, String ssn, double salary) {
+        super(empId, name, ssn, salary);
+    }
+}
